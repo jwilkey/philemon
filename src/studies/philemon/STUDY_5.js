b74c83dd95b19cda35b71b915a46cb5f19e9587f
Justin Wilkey <jwilkey@tentkeep.com>
2020-04-04 10:22:21 -0400

    Add interpretations for Phm Study 5

--- src/studies/philemon/STUDY_5.js
+++ src/studies/philemon/STUDY_5.js
@@ -29,19 +29,48 @@ module.exports = {
   },
   interpret: {
     mainPoint: ``,
-    keywords: [],
+    keywords: {
+      fellow: 'This is a keyword throughout the letter, as Paul continues to emphasize the togetherness produced by union in Christ',
+      grace: `The comfort of God which transcends all worldly troubles. There is very little which truly offers comfort that can stand the test of any scrutinizing, but we can try chase God's grace all the way to it's end, an end we will never reach because it cannot be exhausted, it cannot be explained away, it cannot be beat. If God be true, which He certainly is, then we who follow Him are truly safe.`,
+      spirit: `Here referring to our humana spirit, that indwelling, invisible quality of our existence as creatures made in the image of God. Though our body be thrown in prison, our spirit is free in Christ.`
+    },
     points: [
+      `Paul had friends with him, specifically Epaphras, Mark, Aristarchus, Demas and Luke`,
+      `Paul wanted Philemon to experience Jesus' grace in his spirit`
     ],
     titles: [
+      `Goodbye Philemon`,
+      `We pray you well`
     ],
     emotions: {
+      friendly: 'Paul had several with him, to care for him, and they wished Christ-friendship on Philemon and his church',
+      confidence: `Paul confidently imparted grace upon Philemon, and not just any grace, but the grace of the One Living God`,
+      graciousness: `Paul ends his letter on the most kind and loving of notes, the harmonious, perfect triad of God's grace`
     },
     unwisdom: [
       {
-        passage: 'PHILEMON.17',
-        saying: ``,
-        comment: ``,
-        chapter: ``
+        passage: 'PHILEMON.23-24',
+        saying: `If you're gonna go down, you go down dragging that whole worthless rabble with you!`,
+        comment: `Sheriff, the tumbleweed of your dry, empty heart goes rolling on. Paul often led his friends into unfavorable circumstances per the worlds standards, but he certainly was not doing so out of dejected bitterness, but because their hearts were knit together, because they were fellow warriors in the campaign for the glory of the High King. They were victors in life and triumphant even to death.`,
+        chapter: `Sheriff Unwisdom`
+      },
+      {
+        passage: 'PHILEMON.23-25',
+        saying: `I've wandered these 7 lands and never found a man worth one of my rotten toenails. There is no brother, there is only the parasite.`,
+        comment: `Somebody give the man a hug! The love Christ is rich and free! His love is a lightning bolt shocking even the most dead heart back to life. The man alive in Christ is truly empowered to live in brotherhood, in fellowship that endures the darkest trial.`,
+        chapter: `The Wanderer`
+      },
+      {
+        passage: 'PHILEMON.23-24',
+        saying: `Oh dearest love that fills my soul! It would be better to shrivel in a dark, damp dungeon than to forsake the warm dripping touch of strong love.`,
+        comment: `Oh sweet fruit that poisons and kills! Paul's friendship and mission was not characterized by infatuation or wishful kinship, but by the most true and eternal bond that is formed only fellow mission for Christ.`,
+        chapter: `Lady Aloof`
+      },
+      {
+        passage: 'PHILEMON.25',
+        saying: `I send you good vibes.`,
+        comment: `Oh thank goodness! I was really worried that my illness was going to be my ruin, but now that I know you are sending me the smooth, jazzy undulations of hypothetical goodness, I know I will... I will be... be something that I can't define too clearly or else I might step on someone's toes! Maybe I will drink the cup of grace that is filled by the Powerful Creator of all things, the one who truly sustains us all.`,
+        chapter: `The Relativist`
       }
     ],
     expound: [
